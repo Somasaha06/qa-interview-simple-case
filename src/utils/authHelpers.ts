ea139f5da3246a8f6aa96fdd1a2845135e801b87
Vivek Pramanick <vivekpramanick@Viveks-Air.lan>
2025-04-03 18:48:10 +0200

    modified and added tests

---
+++ src/utils/authHelpers.ts
@@ -0,0 +1,17 @@
+import { Page } from '@playwright/test';
+import { existingUsers } from '../../test-setup/localstorage.setup'
+
+export async function logIn(page: Page, email: string, password: string) {
+  await page.goto('http://localhost:8080/login');
+  //const existingUser = existingUsers[0]
+  await page
+      .locator('#root form div:nth-child(1) > div > input')
+      .pressSequentially(email)
+      await page
+      .locator('#root form div:nth-child(2) > div > input')
+      .pressSequentially(password)
+  // Submit button
+  const button = page.locator('form .MuiButton-sizeMedium')
+  // Click on the button
+  button.click()
+}
